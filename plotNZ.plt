fb8836dbd4ffd7eb13ea40891e7e81f959f30340
Satoko Saita <saita@kct.ac.jp>
2018-09-13 15:25:47 +0900

    Modified for NZ data

--- plotNZ.plt
+++ plotNZ.plt
@@ -3,12 +3,12 @@
 set xdata time
 set timefmt "%Y/%m/%d %H:%M:%S"
 set format x "%H:%M:%S"
-set yrange [32440:33095]
+# set yrange [32440:33095]
 
 set multiplot layout 3,1
-plot "20170829_1608.txt" using 1:3 with lines
-plot "20170829_1608.txt" using 1:4 with lines
-plot "20170829_1608.txt" using 1:5 with lines
+plot "2018-06-06_12_05.txt" using 1:3 with lines
+plot "2018-06-06_12_05.txt" using 1:4 with lines
+plot "2018-06-06_12_05.txt" using 1:5 with lines
 unset multiplot
 if (exist("FRAG") == 0) \
      pause -1 "Click OK to make EPS file";\
--- readRP.c
+++ readRP.c
@@ -5,17 +5,22 @@ int readBuf(int n,FILE *fp);
 
 int main(void){
   FILE *fp1,*fp2;
-  char *fname1 = "/Users/sato/Downloads/2017-08-29_16_08.txt";
-  char *fname2 = "/Users/sato/Downloads/20170829_1608.txt";
+  char *fname1 = "/Users/saita/Dropbox/data/X02/2018/06/06/2018-06-06_12_05.txt";
+  char *fname2 = "./2018-06-06_12_05.txt";
   char buf[100];
   char c='A';
+  int index;
   int i=0,j;
-  int YY,MM,DD,hh,mm,ss,HHH,DDD,ZZZ;
-  
+  int YY,MM,DD,hh,mm,ss,HHH,DDD,ZZZ,GPS,LAT,LONG;
+	double H,D,Z;
   fp1 = fopen( fname1, "r" );
   fp2 = fopen( fname2, "w" );
   if( fp1 == NULL ){
-    printf( "%sファイルが開けません¥n", fname1 );
+    printf( "%s元データファイルが開けません¥n", fname1 );
+    return -1;
+  }
+  if( fp2 == NULL ){
+    printf( "%s変換後データファイルが開けません¥n", fname2 );
     return -1;
   }
 
@@ -28,37 +33,53 @@ int main(void){
     }
     if(c==EOF) break;
 
-    /* skip 1 character */
-    c=fgetc(fp1);
-    printf("c:%c\n",c);
+	/* if first character is 1, the magnetic field data is in this line skip 1 character */
+	index=readBuf(1,fp1);
+		if(index == 1){
+//    printf("c:%c\n",c);
     
     YY=2000+readBuf(2,fp1);
-    printf("YY:%d\n",YY);
+//    printf("YY:%d\n",YY);
   
     MM=readBuf(2,fp1);
-    printf("MM:%d\n",MM);
+//    printf("MM:%d\n",MM);
 
     DD=readBuf(2,fp1);
-    printf("DD:%d\n",DD);
+//    printf("DD:%d\n",DD);
 
     hh=readBuf(2,fp1);
-    printf("hh:%d\n",hh);
+//    printf("hh:%d\n",hh);
     
     mm=readBuf(2,fp1);
-    printf("mm:%d\n",mm);
+//    printf("mm:%d\n",mm);
     
     ss=readBuf(2,fp1);
-    printf("ss:%d\n",ss);
+//	printf("ss:%d\n",ss);
+	
+
   
-    HHH=readBuf(5,fp1);
-    DDD=readBuf(5,fp1);
-    ZZZ=readBuf(5,fp1);
-    printf("HHH:%d\n",HHH);
+	HHH=readBuf(5,fp1);
+	H = (HHH-(2^15))/(2^15)*10.24/10*600;
+	DDD=readBuf(5,fp1);
+	D = (DDD-(2^15))/(2^15)*10.24/10*600;
+	ZZZ=readBuf(5,fp1);
+	Z = (ZZZ-(2^15))/(2^15)*10.24/10*600;
+/*    printf("HHH:%d\n",HHH);
     printf("DDD:%d\n",DDD);
     printf("ZZZ:%d\n",ZZZ);
-
-    fprintf(fp2,"%d/%d/%d %d:%d:%d %d %d %d\n",YY,MM,DD,hh,mm,ss,HHH,DDD,ZZZ);
-
+*/		
+	printf("%c:%d-%02d-%02d %02d:%02d:%02d H: %f, D: %f, Z:%f\n",c,YY,MM,DD,hh,mm,ss,H,D,Z);
+		
+    fprintf(fp2,"%d/%02d/%02d %02d:%02d:%02d %f %f %f\n",YY,MM,DD,hh,mm,ss,H,D,Z);
+		}
+		else{
+			GPS=readBuf(15,fp1);
+			c=fgetc(fp1);
+			LAT=readBuf(6,fp1);
+			c=fgetc(fp1);
+			LONG=readBuf(6,fp1);
+			printf("LAT: %d, LONG: %d\n",LAT,LONG);
+		}
   }
 
   /*  while( (c = fgetc( fp1 )) != EOF ){
@@ -66,7 +87,7 @@ int main(void){
   }
   */
   fclose( fp1 );
-  fclose( fp2 );
+//  fclose( fp2 );
   printf( "\n-- end : readRP --\n" );
 
   return 0;
